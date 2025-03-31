2c682ca192d8fc263a74e82fb5c0c0723eaaf818
safayyas <sophia.shev@gmail.com>
2025-04-01 01:13:23 +0200

    add js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,18 @@
-function getElementWidth(content, padding, border) {
-  content = parseFloat(content);
-  padding = parseFloat(padding);
-  border = parseFloat(border);
-  const result = content + padding * 2 + border * 2;
-  return result;
+function checkForSpam(message) {
+  const lowercaseMessage = message.toLowerCase();
+  if (lowercaseMessage.includes("spam")) {
+    return true;
+  } else if (lowercaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
