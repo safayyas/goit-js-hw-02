a9392a33a701d8ff6f3a2ee0c50f85657c1fcce4
Sofiia <sophia.shev@gmail.com>
2025-03-31 22:46:23 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+  content = parseFloat(content);
+  padding = parseFloat(padding);
+  border = parseFloat(border);
+  const result = content + padding * 2 + border * 2;
+  return result;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
